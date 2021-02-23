efa7ebf71edd501a2ee9e3ce2fc07f69ddf41f3e
ferdiazboy <ferdiazboy@gmail.com>
2021-02-23 16:05:21 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //veri tabanında yapılacak operasyonları içerir - silme,güncelleme vs.
+    public interface IProductDal : IEntityRepository<Product>
+    {
+    }
+    
+}
